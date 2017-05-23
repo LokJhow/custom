eff6004ac5e31991ec34425007352ff1549f5e4e
LokJhow <lokjhow@gmail.com>
2017-05-23 03:14:52 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -49,7 +49,7 @@
       botName: "basicBot",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      scriptLink: "https://rawgit.com/LokJhow/custom/master/extension.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
