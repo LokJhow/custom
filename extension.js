8c510d59d6249ae16f424878498fae89092dafb4
LokJhow <lokjhow@gmail.com>
2017-05-23 01:55:14 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,13 +46,13 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "SOGBot",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/LokJhow/custom/master/lang/pt-BR.json",
+      scriptLink: "https://rawgit.com/LokJhow/custom/master/extension.js",
       roomLock: false, // Requires an extension to re-load the script
-      startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
+      startupCap: 50, // 1-200
+      startupVolume: 100, // 0-100
       startupEmoji: false, // true or false
       autowoot: true,
       autoskip: false,
@@ -105,9 +105,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/LokJhow/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/LokJhow/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/LokJhow/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
