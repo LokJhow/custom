f6e8b4d0ab2d0bb6a26061bd159dbca9befd6c1e
LokJhow <lokjhow@gmail.com>
2017-05-23 02:08:18 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -47,7 +47,7 @@
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "SOGBot",
-      language: "english",
+      language: "portuguese",
       chatLink: "https://rawgit.com/LokJhow/custom/master/lang/pt-BR.json",
       scriptLink: "https://rawgit.com/LokJhow/custom/master/extension.js",
       roomLock: false, // Requires an extension to re-load the script
