8ff4f50446c467294d025bdbdcb066c960475638
LokJhow <lokjhow@gmail.com>
2017-05-23 03:18:16 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,14 +46,14 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "SOGBot",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/LokJhow/custom/master/extension.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
-      startupEmoji: false, // true or false
+      startupVolume: 100, // 0-100
+      startupEmoji: true, // true or false
       autowoot: true,
       autoskip: false,
       smartSkip: true,
@@ -72,7 +72,7 @@
       voteSkipLimit: 10,
       historySkip: false,
       timeGuard: true,
-      maximumSongLength: 10,
+      maximumSongLength: 5,
       autodisable: false,
       commandCooldown: 30,
       usercommandsEnabled: true,
@@ -95,19 +95,19 @@
       etaRestriction: false,
       welcome: true,
       opLink: null,
-      rulesLink: null,
+      rulesLink: ["http://www.soltaosgrave.com/p/plugdj.html"],
       themeLink: null,
       fbLink: null,
       youtubeLink: null,
       website: null,
-      intervalMessages: [],
+      intervalMessages: ["Antes de começar a interagir no chat e entrar na file de DJs leia as regras!!!, acesse: http://www.soltaosgrave.com/p/plugdj.html"],
       messageInterval: 5,
-      songstats: true,
+      songstats: false,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/LokJhow/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/LokJhow/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/LokJhow/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
